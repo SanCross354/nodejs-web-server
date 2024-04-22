8c0cc53ab6817ead761ebbc9dc75ab82ec3374cb
wildan <21523222@students.uii.ac.id>
2024-04-22 11:24:01 +0700

    Adding Method/Verb Request & Body Request

--- server.js
+++ server.js
@@ -4,9 +4,38 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+
+    // An Property to get method's value from request
+    // const method = request.method
+    //     or
+    const { method } = request;
+
+    if (method === "GET") {
+        response.end('<h1>Hello!</h1>');
+    }
+
+    if (method === "POST") {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body.response = Buffer.concat(body).toString;
+            const { name, age } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!, I'm ${age}</h1>`);
+        });
+    }
+
+    // if (method === "PUT") {
+    //     response.end('<h1>Bonjour!</h1>');
+    // }
+
+    // if (method === "DELETE") {
+    //     response.end('<h1>Salam!</h1>');
+    // }
 };
 
 
